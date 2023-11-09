85b7c3284b6a3ddffdfaf719d987842a99fc230a
Jose Ramses Aldama <126728784+JRamses11@users.noreply.github.com>
2023-11-08 22:22:56 -0700

    Add files via upload

---
+++ Interfaz Grafica Usuario C#/CitasMedicasGUI/CitasMedicasGUI/ConsultaTotalMedicos.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace CitasMedicasGUI
+{
+    partial class frmConsultaTotalMedicos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvMedicos = new System.Windows.Forms.DataGridView();
+            this.Clave = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMedicos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvMedicos
+            // 
+            this.dgvMedicos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMedicos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Clave,
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Nombre,
+            this.Column4});
+            this.dgvMedicos.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvMedicos.Location = new System.Drawing.Point(0, 0);
+            this.dgvMedicos.Name = "dgvMedicos";
+            this.dgvMedicos.Size = new System.Drawing.Size(693, 366);
+            this.dgvMedicos.TabIndex = 0;
+            // 
+            // Clave
+            // 
+            this.Clave.HeaderText = "Clave";
+            this.Clave.Name = "Clave";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Nombre";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 150;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Especialidad";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Subespecialidad";
+            this.Column3.Name = "Column3";
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Telefono";
+            this.Nombre.Name = "Nombre";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Consultorio";
+            this.Column4.Name = "Column4";
+            // 
+            // frmConsultaTotalMedicos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 366);
+            this.Controls.Add(this.dgvMedicos);
+            this.Name = "frmConsultaTotalMedicos";
+            this.Text = "ConsultaTotalMedicos";
+            this.Load += new System.EventHandler(this.frmConsultaTotalMedicos_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMedicos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvMedicos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Clave;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+    }
+}
---
+++ Interfaz Grafica Usuario C#/CitasMedicasGUI/CitasMedicasGUI/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿
+namespace CitasMedicasGUI
+{
+    partial class frmClinicaMedica
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblClave = new System.Windows.Forms.Label();
+            this.cmbClave = new System.Windows.Forms.ComboBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblEspecialidad = new System.Windows.Forms.Label();
+            this.lblSubespecialidad = new System.Windows.Forms.Label();
+            this.lblTelefono = new System.Windows.Forms.Label();
+            this.lblConsultorio = new System.Windows.Forms.Label();
+            this.txtEspecialidad = new System.Windows.Forms.TextBox();
+            this.txtSubespecialidad = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.txtConsultorio = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblClave
+            // 
+            this.lblClave.AutoSize = true;
+            this.lblClave.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClave.Location = new System.Drawing.Point(130, 106);
+            this.lblClave.Name = "lblClave";
+            this.lblClave.Size = new System.Drawing.Size(45, 18);
+            this.lblClave.TabIndex = 0;
+            this.lblClave.Text = "Clave";
+            // 
+            // cmbClave
+            // 
+            this.cmbClave.FormattingEnabled = true;
+            this.cmbClave.Location = new System.Drawing.Point(189, 103);
+            this.cmbClave.Name = "cmbClave";
+            this.cmbClave.Size = new System.Drawing.Size(145, 21);
+            this.cmbClave.TabIndex = 1;
+            this.cmbClave.SelectedIndexChanged += new System.EventHandler(this.cmbClave_SelectedIndexChanged);
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(113, 142);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(62, 18);
+            this.lblNombre.TabIndex = 2;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Enabled = false;
+            this.txtNombre.Location = new System.Drawing.Point(189, 142);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(145, 20);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // lblEspecialidad
+            // 
+            this.lblEspecialidad.AutoSize = true;
+            this.lblEspecialidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEspecialidad.Location = new System.Drawing.Point(84, 182);
+            this.lblEspecialidad.Name = "lblEspecialidad";
+            this.lblEspecialidad.Size = new System.Drawing.Size(91, 18);
+            this.lblEspecialidad.TabIndex = 4;
+            this.lblEspecialidad.Text = "Especialidad";
+            // 
+            // lblSubespecialidad
+            // 
+            this.lblSubespecialidad.AutoSize = true;
+            this.lblSubespecialidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSubespecialidad.Location = new System.Drawing.Point(60, 220);
+            this.lblSubespecialidad.Name = "lblSubespecialidad";
+            this.lblSubespecialidad.Size = new System.Drawing.Size(115, 18);
+            this.lblSubespecialidad.TabIndex = 5;
+            this.lblSubespecialidad.Text = "Subespecialidad";
+            // 
+            // lblTelefono
+            // 
+            this.lblTelefono.AutoSize = true;
+            this.lblTelefono.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTelefono.Location = new System.Drawing.Point(109, 260);
+            this.lblTelefono.Name = "lblTelefono";
+            this.lblTelefono.Size = new System.Drawing.Size(66, 18);
+            this.lblTelefono.TabIndex = 6;
+            this.lblTelefono.Text = "Telefono";
+            // 
+            // lblConsultorio
+            // 
+            this.lblConsultorio.AutoSize = true;
+            this.lblConsultorio.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConsultorio.Location = new System.Drawing.Point(90, 302);
+            this.lblConsultorio.Name = "lblConsultorio";
+            this.lblConsultorio.Size = new System.Drawing.Size(85, 18);
+            this.lblConsultorio.TabIndex = 7;
+            this.lblConsultorio.Text = "Consultorio";
+            // 
+            // txtEspecialidad
+            // 
+            this.txtEspecialidad.Enabled = false;
+            this.txtEspecialidad.Location = new System.Drawing.Point(189, 180);
+            this.txtEspecialidad.Name = "txtEspecialidad";
+            this.txtEspecialidad.Size = new System.Drawing.Size(145, 20);
+            this.txtEspecialidad.TabIndex = 8;
+            // 
+            // txtSubespecialidad
+            // 
+            this.txtSubespecialidad.Enabled = false;
+            this.txtSubespecialidad.Location = new System.Drawing.Point(189, 218);
+            this.txtSubespecialidad.Name = "txtSubespecialidad";
+            this.txtSubespecialidad.Size = new System.Drawing.Size(145, 20);
+            this.txtSubespecialidad.TabIndex = 9;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Enabled = false;
+            this.txtTelefono.Location = new System.Drawing.Point(189, 258);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(145, 20);
+            this.txtTelefono.TabIndex = 10;
+            // 
+            // txtConsultorio
+            // 
+            this.txtConsultorio.Enabled = false;
+            this.txtConsultorio.Location = new System.Drawing.Point(189, 300);
+            this.txtConsultorio.Name = "txtConsultorio";
+            this.txtConsultorio.Size = new System.Drawing.Size(145, 20);
+            this.txtConsultorio.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(140, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(203, 24);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "CONSULTA MEDICOS";
+            // 
+            // frmClinicaMedica
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 415);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtConsultorio);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtSubespecialidad);
+            this.Controls.Add(this.txtEspecialidad);
+            this.Controls.Add(this.lblConsultorio);
+            this.Controls.Add(this.lblTelefono);
+            this.Controls.Add(this.lblSubespecialidad);
+            this.Controls.Add(this.lblEspecialidad);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.cmbClave);
+            this.Controls.Add(this.lblClave);
+            this.Name = "frmClinicaMedica";
+            this.Text = "Clinica Medica";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblClave;
+        private System.Windows.Forms.ComboBox cmbClave;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblEspecialidad;
+        private System.Windows.Forms.Label lblSubespecialidad;
+        private System.Windows.Forms.Label lblTelefono;
+        private System.Windows.Forms.Label lblConsultorio;
+        private System.Windows.Forms.TextBox txtEspecialidad;
+        private System.Windows.Forms.TextBox txtSubespecialidad;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.TextBox txtConsultorio;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
