9e27aa1bfac900e6bb8f11e1ba2045cee5e1dd6a
Jose Ramses Aldama <126728784+JRamses11@users.noreply.github.com>
2023-11-08 22:23:36 -0700

    Add files via upload

---
+++ Interfaz Grafica Usuario C#/DatosEmpleadosGUI/DatosEmpleadosGUI/Empleado.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatosEmpleadosGUI
+{
+    public class Empleado
+    {
+        private string Nombre;
+        private string Apellido;
+        private string Ciudad;
+
+        public Empleado(string Nombre, string Apellido, string Ciudad)
+        {
+            this.Nombre = Nombre;
+            this.Apellido = Apellido;
+            this.Ciudad = Ciudad;
+        }
+
+        public string pNombre
+        {
+            set
+            {
+                Nombre = value;
+            }
+            get
+            {
+                return Nombre;
+            }
+        }
+
+        public string pApellido
+        {
+            set
+            {
+                Apellido = value;
+            }
+            get
+            {
+                return Apellido;
+            }
+        }
+
+        public string pCiudad
+        {
+            set
+            {
+                Ciudad = value;
+            }
+            get
+            {
+                return Ciudad;
+            }
+        }
+
+    }
+}
